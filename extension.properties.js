3251b55867132e8a089151bc46cf812a8ff6c022
Víctor J. <maximovj@outlook.com>
2026-02-20 02:11:02 -0600

    * Se instala dotenv y se agrega archivo .env y .env.example
    feature:
    - Se agrega env, isDev, isProd
    - Se establece propiedades según modo dev o prod

--- extension.properties.js
+++ extension.properties.js
@@ -1,16 +1,29 @@
 import pkg from './package.json';
+import dotenv  from 'dotenv';
+
+dotenv.config();
+const env =  process.env.VITE_EXTENSION_MODE || 'dev';
+const isProd = env === 'production' || env === 'prod';
+const isDev = env === 'dev' || env === 'local';
+const name = isProd ? 'QFill' : 'QFill (Dev)';
+const host_permissions = isDev ? ['http://*/*'] : ['http://*/*', 'https://*/*'];
 
 export default {
-    name: 'QFill (Dev)',
+    name,
     author_name: pkg.author.name,
     version: pkg.version,
     description: pkg.description,
-    default_title: 'QFill (Dev) : ' + pkg.description,
-    host_permissions: ['http://*/*', 'https://*/*'],
+    default_title: `${name} : ${pkg.description}`,
+    host_permissions,
     permissions: [
         'activeTab',
         'scripting',
         'sidePanel',
         'contentSettings',
-    ]
+    ],
+
+    // Desarrollo
+    env,
+    isDev,
+    isProd
 }

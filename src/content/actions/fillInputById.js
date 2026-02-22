8ede6b76c8b7485e710f2c1e481cccb95b2d02c4
Víctor J. <maximovj@outlook.com>
2026-02-22 03:36:59 -0600

    * Se agrega try...catch para omitir al rellenar inputs con datos incorrectos

--- src/content/actions/fillInputById.js
+++ src/content/actions/fillInputById.js
@@ -1,35 +1,37 @@
 export default function fillInputById(autofillId, value) {
-    const el = document.querySelector(`[data-autofill-id="${autofillId}"]`);
-    if (!el) return;
+    try {
+        const el = document.querySelector(`[data-autofill-id="${autofillId}"]`);
+        if (!el) return;
 
-    switch (el.type) {
-        case 'checkbox':
-            el.checked = !!value;
-            break;
+        switch (el.type) {
+            case 'checkbox':
+                el.checked = !!value;
+                break;
 
-        case 'radio':
-            if (el.value === value) el.checked = true;
-            break;
+            case 'radio':
+                if (el.value === value) el.checked = true;
+                break;
 
-        case 'select-one':
-            el.value = value;
-            break;
+            case 'select-one':
+                el.value = value;
+                break;
 
-        case 'select-multiple':
-            Array.from(el.options).forEach(opt => {
-                opt.selected = value.includes(opt.value);
-            });
-            break;
+            case 'select-multiple':
+                Array.from(el.options).forEach(opt => {
+                    opt.selected = value.includes(opt.value);
+                });
+                break;
 
-        case 'number':
-        case 'range':
-            el.valueAsNumber = Number(value);
-            break;
+            case 'number':
+            case 'range':
+                el.valueAsNumber = Number(value);
+                break;
 
-        default:
-            el.value = value;
-    }
+            default:
+                el.value = value;
+        }
 
-    el.dispatchEvent(new Event('input', { bubbles: true }));
-    el.dispatchEvent(new Event('change', { bubbles: true }));
+        el.dispatchEvent(new Event('input', { bubbles: true }));
+        el.dispatchEvent(new Event('change', { bubbles: true }));
+    } catch (error) { }    
 }

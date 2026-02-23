64ea74fb9733bd594d5df38d53d449181499414c
Víctor J. <maximovj@outlook.com>
2026-02-22 18:18:13 -0600

    * Se mejora el rellenado de inputs + elementos
    feature:
    - Se agrega rellenado para elementos:
    * span
    * h1
    * div
    * i
    * p
    * label
    * a (solo el texto visible)

--- src/content/actions/fillInputById.js
+++ src/content/actions/fillInputById.js
@@ -1,37 +1,58 @@
-export default function fillInputById(autofillId, value) {
+export default function fillElementById(autofillId, value) {
     try {
         const el = document.querySelector(`[data-autofill-id="${autofillId}"]`);
         if (!el) return;
 
-        switch (el.type) {
-            case 'checkbox':
-                el.checked = !!value;
-                break;
+        if (el.isContentEditable) {
+            el.textContent = value;
+        }
 
-            case 'radio':
-                if (el.value === value) el.checked = true;
-                break;
+        else if (el.tagName === 'TEXTAREA') {
+            el.value = value;
+        }
 
-            case 'select-one':
-                el.value = value;
-                break;
-
-            case 'select-multiple':
+        else if (el.tagName === 'SELECT') {
+            if (el.multiple) {
                 Array.from(el.options).forEach(opt => {
                     opt.selected = value.includes(opt.value);
                 });
-                break;
-
-            case 'number':
-            case 'range':
-                el.valueAsNumber = Number(value);
-                break;
-
-            default:
+            } else {
                 el.value = value;
+            }
+        }
+
+        else if (el.tagName === 'INPUT') {
+            switch (el.type) {
+                case 'checkbox':
+                    el.checked = !!value;
+                    break;
+
+                case 'radio':
+                    if (el.value === value) el.checked = true;
+                    break;
+
+                case 'number':
+                case 'range':
+                    el.valueAsNumber = Number(value);
+                    break;
+
+                case 'file':
+                    return;
+
+                default:
+                    el.value = value;
+            }
+        }
+
+        // 🔥 Cualquier otro elemento HTML
+        else {
+            el.textContent = value;
         }
 
         el.dispatchEvent(new Event('input', { bubbles: true }));
         el.dispatchEvent(new Event('change', { bubbles: true }));
-    } catch (error) { }    
+
+    } catch (error) {
+        console.error(error);
+    }
 }

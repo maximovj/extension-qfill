ba2edac54c72d222be060c3b0c83266dab97e54f
Víctor J. <maximovj@outlook.com>
2026-02-23 01:06:14 -0600

    * Se agrega un debug en background para mostrar datos almacenados solo en desarrollo

--- src/background/main.js
+++ src/background/main.js
@@ -1,18 +1,23 @@
 import extConfig from '../extension.config.js'
-import handleMessages from './handleMessages.js';
+import handleMessages from './handleMessages.js'
+import extensionState from '../extensionState.config.js'
 
 if(extConfig.isDev) {
     // Modo de desarrollo
     const timestamps = new Date().toISOString(); 
     console.log('Inicializando [SERVICE WORKER] extensión QFill (DEV)... ');
     console.log('Timestamps: ' + timestamps);
+    (async () => {
+        //await extensionState.reset();
+        await extensionState.debugEstado();
+    })();
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     (async () => {
         try {
             const resultado = await handleMessages(message, sender);
-            console.log("Resultado de background:", resultado);
+            console.log("Resultado de background:", {message, resultado});
             sendResponse(resultado);
         } catch (error) {
             console.log("Hubo un error en background:", error);

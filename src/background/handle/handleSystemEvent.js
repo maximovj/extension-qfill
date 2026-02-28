49fda6a356f25f2763cc475736b7867449e82fe7
Víctor J. <maximovj@outlook.com>
2026-02-27 22:16:40 -0600

    * Se crea acción para evento de sincronización

--- src/background/handle/handleSystemEvent.js
+++ src/background/handle/handleSystemEvent.js
@@ -5,6 +5,10 @@ export default async function handleSystemEvent(msg) {
     
     switch(msg.action) {
         case ACTIONS.CONNECT: {
+            return { status: 'ok', msg: "Conexión establecida correctamente" };
+        }
+        
+        case ACTIONS.ASYNC_CONFIG: {
             const stateDB = await db.get();
             return { status: 'ok', msg: stateDB };
         }

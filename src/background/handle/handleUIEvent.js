2113676c00c0846880c1bbde77ee8ebfd89c5555
Víctor J. <maximovj@outlook.com>
2026-03-07 01:48:33 -0600

    * Se modifica eventos de handleUIEvent
    feature:
    - Se utiliza db para persistir datos
    - Se modifica enventos
    * ACTIONS.SELECTOR_MODE_SET_ITEM
    * ACTIONS.SELECTOR_MODE_ENABLE

--- src/background/handle/handleUIEvent.js
+++ src/background/handle/handleUIEvent.js
@@ -1,5 +1,5 @@
 import { ACTIONS, MESSAGE_TYPES } from '../../constants.config';
-import { sendToActiveTab, dispatchToActiveTab, sendMessage } from '../../helpers.config';
+import { sendToActiveTab, dispatchToActiveTab, sendMessage, sendToBackground } from '../../helpers.config';
 import extensionState from '../../extensionState.config';
 import db from '../../indexedDBManager';
 
@@ -7,9 +7,7 @@ export default async function handleUIEvent(msg) {
     const stateLocal = await chrome.runtime.sendMessage({
         type: "GET_STATE"
     });
-    const storeConfig = stateLocal?.configuracion;
     const storePerfiles = await db.get(db.STORES.PERFILES);
-    let configDB = {};
     
     switch(msg.action) {
         case ACTIONS.FILL_INPUT_BY_ID: 
@@ -34,21 +32,7 @@ export default async function handleUIEvent(msg) {
         
         case ACTIONS.SELECTOR_MODE_ENABLE: {
             try {
-                console.log("ACTIONS.SELECTOR_MODE_ENABLE", { msg });
-                const { modoAccion } = msg?.payload;
-                await db.set(db.STORES.CONFIGURACION, {
-                    ...storeConfig,
-                    actualizado: Date.now(),
-                    elementoSeleccionado: {},
-                    modo: "selector",
-                    selectorActivado: true,
-                    selectorAccion: modoAccion || "agregar",
-                });
-                await dispatchToActiveTab(
-                    MESSAGE_TYPES.UI_EVENT,
-                    ACTIONS.SELECTOR_MODE_ENABLE
-                );
-                
+                await sendToActiveTab(msg);
                 return { status: 'ok', msg: "Modo selector activado" };
                 //return { status: 'ok', msg: { configDB: {...configDB }, respuesta} };
             } catch (err) {
@@ -60,32 +44,33 @@ export default async function handleUIEvent(msg) {
 
         case ACTIONS.SELECTOR_MODE_SET_ITEM:{
             try {
-                console.log("ACTIONS.SELECTOR_MODE_SET_ITEM", { msg });
+                const getStore = await db.getAll("configuracion");
+                const getState = await db.get();
+                const configuracion = getState?.configuracion || {};
+                const storeConfig = getStore?.at(0) || [];
                 const itemModoSelector = msg?.payload?.data;
                 const elementosActual = storeConfig?.elementos || [];
+                console.log("ACTIONS.SELECTOR_MODE_SET_ITEM", { msg, storeConfig, itemModoSelector, elementosActual, getState });
 
-                if(Array.isArray(elementosActual) && storeConfig) {
-                    await db.set(db.STORES.CONFIGURACION, {
-                        ...storeConfig,
-                        actualizado: Date.now(),
+                if(configuracion?.selectorActivado && configuracion?.modo == "selector" && configuracion?.selectorAccion == "agregar" ){
+                    elementosActual.push(itemModoSelector);
+                    await db.saveSingle(
+                        "configuracion",
+                        {
+                        ...configuracion,
+                        elementoSeleccionado: itemModoSelector,
+                        elementos: elementosActual,
+                        }
+                    );
+                } else {
+                    await db.saveSingle(
+                        "configuracion",
+                        {
+                        ...configuracion,
                         elementoSeleccionado: itemModoSelector,
                         elementos: [itemModoSelector],
-                        modo: "selector",
-                    });
-                    
-                    if(storeConfig?.selectorActivado && storeConfig?.selectorAccion === "agregar") {
-                        elementosActual.push(itemModoSelector);
-                        await db.set(db.STORES.CONFIGURACION, {
-                            ...storeConfig,
-                            actualizado: Date.now(),
-                            elementoSeleccionado: itemModoSelector,
-                            elementos: elementosActual,
-                            modo: "selector",
-                            selectorActivado: storeConfig?.selectorActivado || true,
-                            selectorAccion: storeConfig?.selectorAccion || "agregar",
-                        });
-                    }
-
+                        }
+                    );
                 }
 
                 return { status: 'ok', msg: "Elemento seleccionado en Modo Selector" };
